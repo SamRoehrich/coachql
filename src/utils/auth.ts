9adc20072de5bc98659e0e978b0120e82aea506a
Sam Roehrich <sam_roehrich@icloud.com>
2021-10-03 10:16:24 -0700

    remote db

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -23,7 +23,7 @@ export const createRefreshToken = (user: User) => {
 
 export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
   const authorization = context.req.headers["authorization"];
-  console.log(authorization);
+  console.log(authorization + "is auth");
 
   if (!authorization) {
     throw new Error("not authenticated");
