e50764376b3ef9389edf6a3ec19b6239fc96dc7c
Sam Roehrich <sam_roehrich@icloud.com>
2021-10-26 14:05:16 -0700

    Lunchtime

--- src/resolvers/AssessmentResolver.ts
+++ src/resolvers/AssessmentResolver.ts
@@ -25,6 +25,23 @@ export class AssessmentResolver {
     return org;
   }
 
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth)
+  @UseMiddleware(isInOrg)
+  async deleteAssessment(@Arg("assessmentId") assessmentId: number) {
+    const deleteRes = await getConnection()
+      .createQueryBuilder()
+      .delete()
+      .from(Assessment)
+      .where("id = :id", { id: assessmentId })
+      .execute();
+    if (deleteRes) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   @Query(() => [Assessment])
   async getAssessments() {
     const assessments = await Assessment.find();
@@ -32,6 +49,23 @@ export class AssessmentResolver {
       return assessments;
     }
     return null;
+  }
+
+  @Query(() => [Assessment])
+  @UseMiddleware(isAuth)
+  @UseMiddleware(isInOrg)
+  async getAssessmentsInOrg(@Ctx() context: MyContext) {
+    const assessments = await getRepository(Assessment)
+      .createQueryBuilder()
+      .where("Assessment.organizationId = :orgId", {
+        orgId: context.org?.orgId,
+      })
+      .getMany();
+    if (assessments) {
+      return assessments;
+    } else {
+      return null;
+    }
   }
 
   @Mutation(() => Boolean)
@@ -45,12 +79,14 @@ export class AssessmentResolver {
     @Arg("tools") tools: string,
     @Arg("type") type: string
   ) {
+    const organization = await Organization.findOne(org?.orgId);
     const assessment = await Assessment.insert({
       name,
       description,
       dataPoints,
       testMethod: tools,
       assessmentType: type,
+      organization,
     });
     if (assessment && org) {
       await getConnection()
